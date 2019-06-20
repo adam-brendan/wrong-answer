b73a61667baec17878105b8f4657ecb8610811a8
adam-brendan <adamcraveiro@gmail.com>
2019-06-20 13:50:05 +0000

    Implement wrong answer function and updated README

---
+++ README.md
@@ -0,0 +1,3 @@
+## wrong-answer
+
+Tired of getting the right answer all the time for basic arithmetic? This is the package for you. Simply pass wrongAnswer an integer, basic operator as a string (i.e. "+", "-", "*", or "/") and a second integer and you'll be pleased to see a value that is not the right answer.
---
+++ index.js
@@ -0,0 +1,18 @@
+export const wrongAnswer = (num1, operator, num2) => {
+    let answer = 0;
+    let wrongAnswer = Math.floor(Math.random() * Math.floor(1000));
+    if (operator === "+") {
+        answer = num1 + num2;
+    } else if (operator === "-") {
+        answer = num1 - num2;
+    } else if (operator === "*") {
+        answer = num1 * num2;
+    } else if (operator === "/") {
+        answer = num1 / num2;
+    }
+
+    if (answer === wrongAnswer) {
+        wrongAnswer = Math.floor(Math.random() * Math.floor(1000))
+    }
+    return wrongAnswer;
+}
